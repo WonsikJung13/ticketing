7c61874638d4d97c630bdaa0254529301bbd7027
Limdaram <clara9182@naver.com>
2022-11-30 17:01:20 +0900

    controller, service, mapper, xml, modify.jsp : 비밀번호 변경하기 (modal창)

--- src/main/java/com/limdaram/ticketing/mapper/customer/CustomerMapper.java
+++ src/main/java/com/limdaram/ticketing/mapper/customer/CustomerMapper.java
@@ -10,11 +10,13 @@ public interface CustomerMapper {
 
     CustomerDto getByCustomerUniqueNumber(int customerUniqueNumber);
 
-    int update(CustomerDto customer);
+//    int update(CustomerDto customer);
 
     int delete(CustomerDto customer);
 
     CustomerDto getByCustomerId(String customerId);
 
     CustomerDto getByCustomerEmail(String customerEmail);
+
+    int passwordModify(int customerUniqueNumber, String customerPassword);
 }
